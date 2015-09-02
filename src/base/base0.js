fdc839fa0ec5d58067c6b8fcd1cfa7334a56c8c7
CodeMasterYi <1182096147@qq.com>
2015-09-02 22:04:15 +0800

    update implement of Object.assign

--- src/base/base0.js
+++ src/base/base0.js
@@ -2,11 +2,11 @@
 
 Object.assign = Object.assign || function (target/*, ... sources*/) {
 	if (!target) return target;
-	var arg = arguments.slice(1);
-	for (var i = 0, len = arg.length; i < len; i++) {
-		for (var key in arg[i]) {
-			if (arg[i].hasOwnProperty(key))
-				target[key] = arg[i][key];
+	var sources = arguments.slice(1);
+	for (var i = 0, len = sources.length; i < len; i++) {
+		for (var key in sources[i]) {
+			if (sources[i].hasOwnProperty(key))
+				target[key] = sources[i][key];
 		}
 	}
 	return target;
