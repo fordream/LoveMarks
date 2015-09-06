a08eb3a1cc711af08a7130c3c85fb4ec4052dad4
吴美斌 <1182096147@qq.com>
2015-09-06 11:17:54 +0800

    update base0.js

--- src/base/base0.js
+++ src/base/base0.js
@@ -4,9 +4,10 @@ Object.assign = Object.assign || function (target/*, ... sources*/) {
 	if (!target) return target;
 	var sources = arguments.slice(1);
 	for (var i = 0, len = sources.length; i < len; i++) {
-		for (var key in sources[i]) {
-			if (sources[i].hasOwnProperty(key))
-				target[key] = sources[i][key];
+		var src = sources[i];
+		for (var key in src) {
+			if (src.hasOwnProperty(key))
+				target[key] = src[key];
 		}
 	}
 	return target;
